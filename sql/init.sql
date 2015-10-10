efcb244a045d4efb06c145a6df9141380225a815
yunyu-Mr <yellow-ming@163.com>
2015-10-10 15:47:19 +0800

    add sql

---
+++ sql/init.sql
@@ -0,0 +1,4 @@
+# Available VMs initialization
+DELETE FROM available_vms;
+insert into available_vms values("bddc47a1-24da-6303-e767-fe6defe58657","Cloud_Gaming_VM1","172.18.216.84","172.18.216.179");
+insert into available_vms values("fe69c2a9-6473-d487-612a-744b271b3346","Cloud_Gaming_VM1","172.18.216.33","172.18.216.158");
